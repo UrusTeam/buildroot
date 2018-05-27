848f73b862e05efa9d6b5de8d76cb4decb0ff96f
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2018-05-27 01:36:53 -0400

    board: updated grandprimeve3g urusx and scripts.

--- board/urussystem/samsung/grandprimeve3g/post-build.sh
+++ board/urussystem/samsung/grandprimeve3g/post-build.sh
@@ -6,6 +6,9 @@ BOARD_DIR="$(dirname $0)"
 MKIMAGE=$HOST_DIR/usr/bin/mkimage
 BOOT_CMD=$BOARD_DIR/boot.cmd
 BOOT_CMD_H=$BINARIES_DIR/boot.scr
+URUSX_BIN=$BOARD_DIR/urusx
+
+cp $URUSX_BIN ${TARGET_DIR}
 
 # U-Boot script
 #$MKIMAGE -C none -A arm -T script -d $BOOT_CMD $BOOT_CMD_H
