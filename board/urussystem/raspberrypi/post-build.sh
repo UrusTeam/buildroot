975a0a652590d3ea644bdedaef8143f2c86dad28
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2019-09-09 05:09:28 -0400

    board: urusystem: rpiall: commented inittab login urusapp.

--- board/urussystem/raspberrypi/post-build.sh
+++ board/urussystem/raspberrypi/post-build.sh
@@ -6,6 +6,12 @@ set -e
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
 
+URUS_INITSRC=$BOARD_DIR/S30psplash
+URUS_INITTGT=${TARGET_DIR}/etc/init.d/
+
+URUSX_BINSRC=$BOARD_DIR/urusx
+URUSX_BINTGT=${TARGET_DIR}/usr/bin/
+
 # Add a console on tty1
 if [ -e ${TARGET_DIR}/etc/inittab ]; then
     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
@@ -13,12 +19,15 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::once:/bin/login' ${TARGET_DIR}/etc/inittab || \
-    sed -i '/starturus/a\
-tty1::once:/bin/login -f urusapp # URUS APP user login' ${TARGET_DIR}/etc/inittab
-fi
+#if [ -e ${TARGET_DIR}/etc/inittab ]; then
+#    grep -qE '^tty1::once:/bin/login' ${TARGET_DIR}/etc/inittab || \
+#    sed -i '/starturus/a\
+#tty1::once:/bin/login -f urusapp # URUS APP user login' ${TARGET_DIR}/etc/inittab
+#fi
 
 cp -f $BOARD_DIR/config-$BOARD_NAME.txt $BINARIES_DIR/rpi-firmware/config.txt
 cp -f $BOARD_DIR/cmdline-$BOARD_NAME.txt $BINARIES_DIR/rpi-firmware/cmdline.txt
 cp -f $BOARD_DIR/startapp.sh ${TARGET_DIR}/etc/profile.d/
+
+cp -f $URUSX_BINSRC $URUSX_BINTGT
+cp -f $URUS_INITSRC $URUS_INITTGT
