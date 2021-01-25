aefd51b0c55ef5cd9bcfbdad6724fd5579b297b0
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2021-01-25 08:40:18 -0400

    board: urussystem: rpi: added installing sudoers and weston script to etc.

--- board/urussystem/raspberrypi/post-build.sh
+++ board/urussystem/raspberrypi/post-build.sh
@@ -25,6 +25,9 @@ fi
 #tty1::once:/bin/login -f urusapp # URUS APP user login' ${TARGET_DIR}/etc/inittab
 #fi
 
+cp $BOARD_DIR/10-urus-app-sudoers ${TARGET_DIR}/etc/sudoers.d/
+cp $BOARD_DIR/urus_weston.ini ${TARGET_DIR}/etc/
+
 cp -f $BOARD_DIR/config-$BOARD_NAME.txt $BINARIES_DIR/rpi-firmware/config.txt
 cp -f $BOARD_DIR/cmdline-$BOARD_NAME.txt $BINARIES_DIR/rpi-firmware/cmdline.txt
 cp -f $BOARD_DIR/startapp.sh ${TARGET_DIR}/etc/profile.d/
