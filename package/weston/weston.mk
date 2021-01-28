ad644bc50a7fc04bfccb205e74c9790785fe4cf5
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2021-01-28 07:02:32 -0400

    package: moved weston permissions define on disable weston launch.

--- package/weston/weston.mk
+++ package/weston/weston.mk
@@ -55,6 +55,10 @@ endef
 WESTON_CONF_OPTS += --enable-weston-launch
 WESTON_DEPENDENCIES += linux-pam
 else
+define WESTON_PERMISSIONS
+	/usr/bin/weston f 4755 0 0 - - - - -
+	/usr/bin/weston-terminal f 4755 0 0 - - - - -
+endef
 WESTON_CONF_OPTS += --disable-weston-launch
 endif
 
@@ -97,10 +101,6 @@ WESTON_CONF_OPTS += \
 	--enable-drm-compositor \
 	--with-cairo-glesv2 \
 	WESTON_NATIVE_BACKEND=drm-backend.so
-define WESTON_PERMISSIONS
-	/usr/bin/weston f 4755 0 0 - - - - -
-	/usr/bin/weston-terminal f 4755 0 0 - - - - -
-endef
 else
 WESTON_CONF_OPTS += --disable-drm-compositor
 endif
